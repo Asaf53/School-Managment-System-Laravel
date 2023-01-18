dfff92c82947b269caed50ea5ea4945c1cc3d658
Asaf53 <asafrushiti3@gmail.com>
2023-01-18 19:26:10 +0300

    Every route is working fine

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectClassroomController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
@@ -23,39 +24,48 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
+Auth::routes();
+// Auth::routes(['register' => false]);
 Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::controller(StudentController::class)->prefix('student')->middleware('isAdmin')->group(function () {
     Route::get('index', 'index')->name('showStudent');
     Route::get('add', 'add')->name('addStudent');
+    Route::post('store', 'storeStudent')->name('storeStudent');
     Route::get('edit/{id}', 'edit')->name('editStudent');
-    Route::post('update', 'update')->name('updateStudent');
+    Route::post('update/{id}', 'update')->name('updateStudent');
+    Route::get('delete/{id}', 'delete')->name('deleteStudent');
 });
 Route::controller(TeacherController::class)->prefix('teacher')->middleware('isAdmin')->group(function () {
     Route::get('index', 'index')->name('showTeacher');
     Route::get('add', 'add')->name('addTeacher');
+    Route::post('store', 'storeTeacher')->name('storeTeacher');
     Route::get('edit/{id}', 'edit')->name('editTeacher');
-    Route::post('update', 'update')->name('updateTeacher');
+    Route::post('update/{id}', 'update')->name('updateTeacher');
+    Route::get('delete/{id}', 'delete')->name('deleteTeacher');
 });
 Route::controller(ClassroomController::class)->prefix('classroom')->middleware('isAdmin')->group(function () {
     Route::get('index', 'index')->name('showClassroom');
     Route::get('add', 'add')->name('addClassroom');
+    Route::post('store', 'storeClassroom')->name('storeClassroom');
     Route::get('edit/{id}', 'edit')->name('editClassroom');
-    Route::post('update', 'update')->name('updateClassroom');
+    Route::post('update/{id}', 'update')->name('updateClassroom');
+    Route::get('delete/{id}', 'delete')->name('deleteClassroom');
+});
 
-    Route::get('addSubjectClassroom', 'addSubjectClassroom')->name('addSubjectClassroom');
+Route::controller(SubjectClassroomController::class)->prefix('classroomsubject')->middleware('isAdmin')->group(function () {
+    Route::get('index', 'index')->name('showSubjectClassroom');
+    Route::get('add', 'add')->name('addSubjectClassroom');
+    Route::post('storeSubjectClassroom', 'storeSubjectClassroom')->name('storeSubjectClassroom');
     Route::get('editSubjectClassroom/{id}', 'editSubjectClassroom')->name('editSubjectClassroom');
     Route::post('update', 'update')->name('updateSubjectClassroom');
-
-    Route::get('addStudentClassroom', 'addStudentClassroom')->name('addStudentClassroom');
-    Route::get('editStudentClassroom/{id}', 'editStudentClassroom')->name('editStudentClassroom');
-    Route::post('update', 'update')->name('updateStudentClassroom');
 });
 Route::controller(SubjectController::class)->prefix('subject')->middleware('isAdmin')->group(function () {
     Route::get('index', 'index')->name('showSubject');
     Route::get('add', 'add')->name('addSubject');
+    Route::post('store', 'storeSubject')->name('storeSubject');
     Route::get('edit/{id}', 'edit')->name('editSubject');
-    Route::post('update', 'update')->name('updateSubject');
+    Route::post('update/{id}', 'update')->name('updateSubject');
+    Route::get('delete/{id}', 'delete')->name('deleteSubject');
 });
